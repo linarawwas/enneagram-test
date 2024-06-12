b6bd09ec9aece62744026db9e4d0da713ae2dfb7
linarawwas <lina@stitchesstudios.com>
2024-06-12 15:36:47 +0300

    made the admin route to the admin dashboard

--- INTERNSHIP/enneagram_test/frontend/src/Layout/Layout.jsx
+++ INTERNSHIP/enneagram_test/frontend/src/Layout/Layout.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect } from "react";
-import AdminRouter from "../Router/AdminRouter";
 import Test from "../Pages/UserPages/Test/Test.tsx";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAuthenticatedMe } from "../features/auth/authApi"; // Adjust import to match your structure
 
 function Layout() {
   const dispatch = useDispatch();
-  const is_admin = useSelector((state) => state.auth.me.isAdmin);
+  const isAdmin = useSelector((state) => state.auth.me.isAdmin);
 
   useEffect(() => {
     dispatch(fetchAuthenticatedMe());
   }, [dispatch]);
 
-  return <>{is_admin ? <AdminRouter /> : <Test />}</>;
+  useEffect(() => {
+    if (isAdmin) {
+      window.location.href = "http://localhost:3001/";
+    }
+  }, [isAdmin]);
+
+  return <>{isAdmin ? null : <Test />}</>;
 }
 
 export default Layout;
